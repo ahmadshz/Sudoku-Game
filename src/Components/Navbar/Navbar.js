43306ad5285c5dd8e507a48740dfcdbfd1fb7546
Ahmad Shmeit <110113553+ahmadshz@users.noreply.github.com>
2024-11-22 21:45:18 +0200

    Update Navbar.js

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,9 +2,10 @@ import React, { useContext } from 'react';
 import { IoSunny } from "react-icons/io5";
 import { AiFillMoon } from "react-icons/ai";
 import './Navbar.css';
-import { IsDarkMode } from '../../Context/DakMode';  // Corrected the import
+import { IsDarkMode } from '../../Context/DakMode'; 
 
 const Navbar = () => {
+  // theme context
   const { darkMode, setDarkMode } = useContext(IsDarkMode);
 
   const handleDarkMode = () => {
